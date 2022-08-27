a92908d21dd666c3102986b50cd48e5196b25aea
Qiao Qiao <68757394+qiaouchicago@users.noreply.github.com>
2022-08-26 20:49:28 -0500

    add github workflow for pytest (#6)
    * add github workflow for pytest
    
    * install markdowntoc in github actions
    
    * add repo visualizer
    
    * export all functions
    
    * add pre-commit to github actions
    
    * use setuptools_scm<7 for python3.6
    
    Co-authored-by: repo-visualizer <repo-visualizer@users.noreply.github.com>

--- src/markdowntoc/__init__.py
+++ src/markdowntoc/__init__.py
@@ -1,5 +1,16 @@
 from pkg_resources import get_distribution
 
-from .markdowntoc import main
+from .markdowntoc import (
+    create_github_header_anchor,
+    create_table_of_contents,
+    create_table_of_contents_github,
+    find_toc_end,
+    find_toc_start,
+    get_headers,
+    get_parser,
+    main,
+    sequentialize_header_priorities,
+    write_results,
+)
 
 VERSION = get_distribution(__name__).version
